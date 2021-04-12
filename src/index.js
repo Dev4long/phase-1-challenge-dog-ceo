873fb535625c7b9e8208e1f6e49f469e5c278913
deniskadrich <dev4long@live.com>
2021-04-12 14:57:21 -0400

    commit tree e91ebc5d1ea0b5c15c3012f962bf4e63a89b703d

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 console.log('%c HI', 'color: firebrick')
+
+
+
+fetch("https://dog.ceo/api/breeds/image/random/4")
+.then(response => response.json())
+.then((responsePojo) => {
+    console.log(responsePojo)
+
+
+let blankLi = document.createElement('li')
+let blankImg = document.createElement('img')
+ 
+blankImg.src = responsePojo.message
+
+blankLi.append(blankImg)
+
+})
+
